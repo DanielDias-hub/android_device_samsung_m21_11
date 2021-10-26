f0a47bce2be8984cfb74d2ce6c94175f3a1c2703
Daniel <danielm3301@mitrastudents.com>
2021-10-26 21:09:31 +0530

    Update and rename aosp_m30s.mk to lineage_m21.mk

--- lineage_m21.mk
+++ lineage_m21.mk
@@ -5,7 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/m30s/device.mk)
+$(call inherit-product, device/samsung/m21/device.mk)
 
 ### BOOTANIMATION
 # Boot Animation
@@ -15,23 +15,23 @@ TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 ### AOSP
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_m30s
-PRODUCT_DEVICE := m30s
+PRODUCT_NAME := lineage_m21
+PRODUCT_DEVICE := m21
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-M307F
+PRODUCT_MODEL := SM-M215F
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_SHIPPING_API_LEVEL := 28
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 	
 ## Use the latest CTS approved build identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=m30s \
-    PRIVATE_BUILD_DESC="m30s-user 11 RP1A.200720.012 M307FXXU4CUH6 release-keys"
+    PRODUCT_NAME=m21 \
+    PRIVATE_BUILD_DESC="m21-user 11 RP1A.200720.012 M215FXXU2BUH6 release-keys"
 
-BUILD_FINGERPRINT := samsung/m30sdd/m30s:11/RP1A.200720.012/M307FXXU4CUH6:user/release-keys
+BUILD_FINGERPRINT := samsung/m21dd/m21:11/RP1A.200720.012/M215FXXU2BUH6:user/release-keys
