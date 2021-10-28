ff84aef5ac4fdd8ededf5d08990f91bd38776561
Daniel <danielm3301@mitrastudents.com>
2021-10-28 07:35:38 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,12 +1,12 @@
 ### PLATFORM
-$(call inherit-product, device/samsung/m30s-common/platform.mk)
+$(call inherit-product, device/samsung/21-common/platform.mk)
 ### PROPRIETARY VENDOR FILES
-$(call inherit-product, vendor/samsung/m30s/m30s-vendor.mk)
+$(call inherit-product, vendor/samsung/m21/m21-vendor.mk)
 
 ### DALVIK
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-DEVICE_PATH := device/samsung/m30s
+DEVICE_PATH := device/samsung/m21
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay
