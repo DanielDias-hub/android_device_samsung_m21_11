1d47745f693a3a615be90ae3f68ce6380d9f6bfb
Daniel <danielm3301@mitrastudents.com>
2021-10-29 18:14:41 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,7 +3,11 @@ $(call inherit-product, device/samsung/21-common/platform.mk)
 ### PROPRIETARY VENDOR FILES
 $(call inherit-product, vendor/samsung/m21/m21-vendor.mk)
 
-$ Board_has_dynamic_partitions = true 
+## DYNAMIC PARTITIONS
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+
 
 ### DALVIK
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
